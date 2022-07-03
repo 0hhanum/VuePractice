8ba979177eeda09c9b2e5bf69dedd8b73d603f05
hanum <rntls123@naver.com>
2022-07-04 00:30:40 +0900

    Use Vue App

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,11 +1,38 @@
 <template>
-  <div id="app"></div>
+  <section id="app">
+    <header>
+      <h1>People</h1>
+    </header>
+    <ul>
+      <contact-component
+        v-for="person in people"
+        :key="person.id"
+        :person="person"
+      />
+    </ul>
+  </section>
 </template>
 
 <script>
 const app = {
+  name: "App",
   data() {
-    return {};
+    return {
+      people: [
+        {
+          id: "goodthing",
+          name: "hanum",
+          number: "12 1231",
+          email: "ewef@asd.com",
+        },
+        {
+          id: "hello",
+          name: "hah",
+          number: "12 121",
+          email: "ewesdf@asd.com",
+        },
+      ],
+    };
   },
 };
 export default app;
---
+++ app/src/components/Contact.vue
@@ -0,0 +1,35 @@
+<template>
+  <li>
+    <h2>{{ person.name }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsVisible ? "Hide" : "Show" }} Details
+    </button>
+    <ul v-if="detailVisible">
+      <li><strong>Phone: </strong>{{ person.number }}</li>
+      <li><strong>Email: </strong>{{ person.email }}</li>
+    </ul>
+  </li>
+</template>
+<script>
+export default {
+  name: "contact-component",
+  props: {
+    person: {
+      id: String,
+      name: String,
+      number: String,
+      email: String,
+    },
+  },
+  data() {
+    return {
+      detailVisible: false,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailVisible = !this.detailVisible;
+    },
+  },
+};
+</script>
