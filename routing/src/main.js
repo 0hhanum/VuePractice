4291607ac48d648e84cb7d9a8e7f5d9d40c46ed1
hanum <rntls123@naver.com>
2022-07-31 22:16:24 +0900

    Intercept wrong access

--- routing/src/main.js
+++ routing/src/main.js
@@ -2,19 +2,23 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import App from "./App.vue";
 
-import BeerComponent from "./components/BeerComponent";
 import BeerList from "./BeerList";
+
+import BeerComponent from "./components/BeerComponent";
 import PeerComponent from "./components/PeerComponent";
+
+import NotFound from "./screens/NotFound";
 
 Vue.config.productionTip = false;
 
 const routes = [
   // { path: "/", redirect: "/beer" },
   { path: "/beer", component: BeerList, alias: "/" },
-  // alias doesn't change the URL
+  // ** alias doesn't change the URL
   { path: "/peer", component: PeerComponent },
   { path: "/beer/:beerName", component: BeerComponent, props: true },
   // dynamic parameter 를 prop 으로 받음
+  { path: "/:catchAll(.*)", component: NotFound },
 ];
 const router = new VueRouter({
   mode: "history",
---
+++ routing/src/screens/NotFound.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Wrong Access!</h1>
+    <span>You find <router-link to="/beer">Beer ?</router-link></span>
+  </div>
+</template>
