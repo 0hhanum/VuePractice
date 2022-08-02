7b85f236653878d74e9ecdcf4b18eca8144e2961
hanum <rntls123@naver.com>
2022-08-02 23:45:24 +0900

    Transition component

--- animation/src/App.vue
+++ animation/src/App.vue
@@ -7,12 +7,14 @@
     <div id="secondContainer" class="box">
       <button @click="openDialog">SHOW DIALOG</button>
     </div>
-    <dialog open v-show="dialogOpen">
-      <div id="dialog-button-container">
-        <button @click="closeDialog">X</button>
-      </div>
-      <strong>I AM DIALOG</strong>
-    </dialog>
+    <transition>
+      <dialog open v-show="dialogOpen">
+        <div id="dialog-button-container">
+          <button @click="closeDialog">X</button>
+        </div>
+        <strong>I AM DIALOG</strong>
+      </dialog>
+    </transition>
   </div>
 </template>
 
@@ -32,7 +34,8 @@ export default {
       this.dialogOpen = false;
     },
     animate() {
-      this.animateActivated = !this.animateActivated;
+      this.animateActivated = false;
+      setTimeout(() => (this.animateActivated = true), 100);
     },
   },
 };
@@ -77,7 +80,7 @@ body {
   height: 120px;
   background-color: black;
   margin-bottom: 50px;
-  transition: all 1s ease-in-out;
+  /* transition: all 1s ease-in-out; */
 }
 button {
   border: none;
@@ -91,6 +94,9 @@ dialog {
   margin-top: 200px;
   width: 1200px;
   height: 200px;
+  border-radius: 20px;
+  border: 1px solid rgba(1, 1, 1, 0.4);
+  box-shadow: 2px 2px rgba(1, 1, 1, 0.3);
 }
 #dialog-button-container {
   width: 100%;
@@ -98,6 +104,47 @@ dialog {
   justify-content: end;
 }
 .animate {
-  transform: translateX(-5rem);
+  animation: animation 1s ease-in-out;
+}
+
+/* <transition> 태그 사용 */
+.v-enter {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.5s ease-in-out;
+}
+.v-enter-to {
+  opacity: 1;
+}
+
+.v-leave-to {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+
+@keyframes animation {
+  10% {
+    transform: translateX(-5rem) scale(1);
+  }
+  70% {
+    transform: translateX(5rem) scale(1.5);
+  }
+  100% {
+    transform: translateX(0rem) scale(1);
+  }
+}
+@keyframes modal {
+  from {
+    opacity: 0;
+    transform: scale(0.8);
+  }
+  to {
+    opacity: 1;
+    transform: scale(1);
+  }
 }
 </style>
