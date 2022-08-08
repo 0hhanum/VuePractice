fe0b9650ae7390af3afe0a8fe8b8cee50b41adaf
hanum <rntls123@naver.com>
2022-08-08 23:37:28 +0900

    mapActions

--- vuex/src/App.vue
+++ vuex/src/App.vue
@@ -1,25 +1,33 @@
 <template>
   <div>
-    <div>
-      <MyComponent></MyComponent>
+    <div v-if="false">
+      <div>
+        <MyComponent></MyComponent>
+      </div>
+      <div>
+        <ul>
+          <li v-for="item in items" :key="item">
+            {{ item }}
+          </li>
+        </ul>
+      </div>
+      <hr />
+      <ul>
+        <li v-for="item in storedItems" :key="item">{{ item }}</li>
+      </ul>
+      <hr />
+      <div>
+        <MyComponent2></MyComponent2>
+      </div>
+      <div>
+        <GetState></GetState>
+      </div>
     </div>
     <div>
       <ul>
-        <li v-for="item in items" :key="item">
-          {{ item }}
-        </li>
+        <li v-for="item in storedItems" :key="item">{{ item }}</li>
+        <button @click="addItem({ value: 131313 })">add</button>
       </ul>
-    </div>
-    <hr />
-    <ul>
-      <li v-for="item in storedItems" :key="item">{{ item }}</li>
-    </ul>
-    <hr />
-    <div>
-      <MyComponent2></MyComponent2>
-    </div>
-    <div>
-      <GetState></GetState>
     </div>
   </div>
 </template>
@@ -28,6 +36,7 @@
 import MyComponent from "./components/MyComponent";
 import MyComponent2 from "./components/MyComponent2";
 import GetState from "./components/GetState";
+import { mapActions } from "vuex";
 
 export default {
   components: {
@@ -49,6 +58,9 @@ export default {
     storedItems() {
       return this.$store.state.items;
     },
+  },
+  methods: {
+    ...mapActions(["addItem"]),
   },
 };
 </script>
