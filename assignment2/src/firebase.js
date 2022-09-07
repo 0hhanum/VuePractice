93611265b6a7092d7f96d334b74c21fe56464633
hanum <rntls123@naver.com>
2022-09-08 01:15:58 +0900

    Github action test #38

--- assignment2/src/firebase.js
+++ assignment2/src/firebase.js
@@ -14,15 +14,28 @@ import {
 } from "@firebase/auth";
 import store from "@/store/index";
 
-const firebaseConfig = {
-  apiKey: process.env.VUE_APP_API_KEY,
-  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
-  databaseURL: process.env.VUE_APP_DATABASE_URL,
-  projectId: process.env.VUE_APP_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
-  appId: process.env.VUE_APP_APP_ID,
-};
+console.log(store.getters.getIsLocal);
+const isLocal = store.getters.getIsLocal;
+
+const firebaseConfig = isLocal
+  ? {
+      apiKey: process.env.VUE_APP_API_KEY,
+      authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+      databaseURL: process.env.VUE_APP_DATABASE_URL,
+      projectId: process.env.VUE_APP_PROJECT_ID,
+      storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+      messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+      appId: process.env.VUE_APP_APP_ID,
+    }
+  : {
+      apiKey: import.meta.env.VUE_APP_API_KEY,
+      authDomain: import.meta.env.VUE_APP_AUTH_DOMAIN,
+      databaseURL: import.meta.env.VUE_APP_DATABASE_URL,
+      projectId: import.meta.env.VUE_APP_PROJECT_ID,
+      storageBucket: import.meta.env.VUE_APP_STORAGE_BUCKET,
+      messagingSenderId: import.meta.env.VUE_APP_MESSAGING_SENDER_ID,
+      appId: import.meta.env.VUE_APP_APP_ID,
+    };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const actionCodeSettings = {
